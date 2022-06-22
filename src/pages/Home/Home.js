0b11e6b0ec3a8de6ac59c750da5b34b8adbf71ef
aishatpopoola <aishatpopoola906@gmail.com>
2022-06-23 00:54:13 +0100

    add the feature to add new counts using debounce

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,8 @@
 import Axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import DayCountsTable from '../../component/DayCountsTable/DayCountsTable';
-import { getDayCountsRoute } from '../../utils/routes';
+import { configHeader } from '../../utils/config';
+import { addCountRoute, getDayCountsRoute } from '../../utils/routes';
 import './home.css';
 
 const emptyCountData = {
@@ -15,6 +16,8 @@ const emptyCountData = {
 const Home = () => {
   const [todayCount, setTodayCount] = useState(emptyCountData);
   const [dayCounts, setDayCounts] = useState([]);
+  const [count, setCount] = useState(0);
+  const [debouncedCount, setDebouncedCount] = useState(0);
 
   const getAllDayCounts = () => {
     Axios.get(getDayCountsRoute)
@@ -33,13 +36,53 @@ const Home = () => {
   useEffect(() => {
     getAllDayCounts();
   }, []);
+
+  const addTodaysCount = () => {
+    const payload = { counter: debouncedCount };
+
+    Axios.post(addCountRoute, payload, configHeader)
+      .then((res) => {
+        if (res.data.is_first_time_counting_today && todayCount.status) {
+          setDayCounts([...dayCounts, todayCount]);
+          setTodayCount(res.data.count);
+        }
+        setCount(0);
+        setDebouncedCount(0);
+      })
+      .catch(() => {
+
+      });
+  };
+
+  useEffect(() => {
+    if (debouncedCount) {
+      addTodaysCount();
+    }
+  }, [debouncedCount]);
+
+  useEffect(() => {
+    const countTimer = setTimeout(() => {
+      setDebouncedCount(count);
+    }, 1000);
+    return () => {
+      clearTimeout(countTimer);
+    };
+  }, [count]);
+
+  const incrementCount = () => {
+    setCount((prevState) => prevState + 1);
+    setTodayCount({ ...todayCount, count: todayCount.count + 1 });
+  };
+
   return (
     <div>
       <section>
         <h2 className="counter-text">{todayCount?.count || 0}</h2>
-        <button type="button" className="btn-primary btn">Click Me</button>
+        <button type="button" className="btn-primary btn" onClick={incrementCount}>Click Me</button>
       </section>
-      <DayCountsTable dayCounts={dayCounts} />
+      <section className="section">
+        <DayCountsTable dayCounts={dayCounts} />
+      </section>
     </div>
   );
 };
